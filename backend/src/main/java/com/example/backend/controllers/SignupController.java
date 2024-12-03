5ba7ff545079c99d4eb7c07b9754016e46eea4dd
Armia Joseph <armia.joseph35@gmail.com>
2024-12-03 23:44:15 +0200

    fix : changed to RestController

--- backend/src/main/java/com/example/backend/controllers/SignupController.java
+++ backend/src/main/java/com/example/backend/controllers/SignupController.java
@@ -6,11 +6,9 @@ import com.example.backend.services.SignupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/signup")
 public class SignupController {
     @Autowired
