b1b7d7b061c092b5d7c547074050adc6efaa56f5
Armia Joseph <armia.joseph35@gmail.com>
2024-12-03 20:05:16 +0200

    fix: BOD, Gender datatypes

--- backend/src/main/java/com/example/backend/dtos/UserSignupDTO.java
+++ backend/src/main/java/com/example/backend/dtos/UserSignupDTO.java
@@ -3,14 +3,16 @@ package com.example.backend.dtos;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Date;
+
 @Getter
 @Setter
 public class UserSignupDTO {
     private String firstName;
     private String lastName;
     private String username;
-    private String gender;
-    private String dateOfBirth;
+    private boolean gender;
+    private Date dateOfBirth;
     private String email;
     private String password;
 }
