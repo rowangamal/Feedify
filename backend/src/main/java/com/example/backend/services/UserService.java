78150ea0c067d448aa9fbf90207c2f02212d8b66
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-12-03 19:20:08 +0200

    feat: adding function to get useID from authentication context saved

--- backend/src/main/java/com/example/backend/services/UserService.java
+++ backend/src/main/java/com/example/backend/services/UserService.java
@@ -2,11 +2,15 @@ package com.example.backend.services;
 
 import com.example.backend.entities.Admin;
 import com.example.backend.entities.User;
+import com.example.backend.entities.UserDetail;
 import com.example.backend.enums.Role;
+import com.example.backend.exceptions.UnauthorizedAccessException;
 import com.example.backend.exceptions.UserNotFoundException;
 import com.example.backend.repositories.AdminRepository;
 import com.example.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 
@@ -39,6 +43,20 @@ public class UserService {
             return Role.ADMIN;
         }
         return Role.USER;
+    }
+
+    public Long getUserId(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        /*
+        * If the user is not authenticated or the principal is not an instance of UserDetail, throw an UnauthorizedAccessException
+        * This case should not happen, because it means caller expects authenticated user to be present
+        * We would not have reached this point if the user was not authenticated
+        * but for security reasons, we should check this case
+         */
+        if (authentication == null || !(authentication.getPrincipal() instanceof UserDetail)) {
+            throw new UnauthorizedAccessException("User is not authenticated or invalid principal");
+        }
+        return (Long) ((UserDetail)authentication.getPrincipal()).getUserId();
     }
 
 }
