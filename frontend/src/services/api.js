27266069dd133672ccb6c68b2fa0a444816ad3c9
Armia Joseph <armia.joseph35@gmail.com>
2024-12-04 01:33:34 +0200

    fix: api endpoints

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,31 +3,19 @@ import axios from 'axios';
 const API_BASE_URL = 'http://localhost:8080';
 
 export const signup = async (formData) => {
-    /**
-     * API call to sign up a user.
-     * formData : {
-     *     "firstName": string,
-     *     "lastName": string,
-     *     "username": string,
-     *     "gender": boolean,
-     *     "dateOfBirth": Date,
-     *     "email": string,
-     *     "password": string
-     * }
-     */
-    const url = `${API_BASE_URL}/signup`; 
-    
-    try{
+    const url = `${API_BASE_URL}/signup`;
+
+    try {
         const response = await axios.post(url, formData, {
             headers: {
                 'Content-Type': 'application/json',
             }
         });
+        return response;
     } catch (error) {
         if (error.response && error.response.data) {
-            throw new Error(error.response.data);  
+            throw new Error(error.response.data);
         }
-        throw error;  
+        throw error;
     }
-
 };
