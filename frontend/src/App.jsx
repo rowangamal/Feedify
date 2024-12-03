c6ce3dff293eeafc3eb20a82dc1a9416e3ea76dc
Omar-Eldawy <eng.omar.aldawy@gmail.com>
2024-12-04 01:07:50 +0200

    User profile front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import CreatePost from "./components/CreatePost";
+// import CreatePost from "./components/CreatePost";
 // import Home from "./components/Home";
+import Profile from "./components/UserProfile/Profile";
 function App() {
   return (
     <>
-      <CreatePost />
+      <Profile userId={3} username={"Omar Aldawy"} followers={37} following={77}/>
+      {/* <CreatePost /> */}
       {/* <Home /> */}
     </>
   );
