f548e50c5a1bda1d492ebe6852ad1d51f8ddf0d5
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-12-03 18:41:49 +0200

    refactor: subject from username:id for token, admin auth works

--- backend/src/main/java/com/example/backend/entities/UserDetail.java
+++ backend/src/main/java/com/example/backend/entities/UserDetail.java
@@ -59,4 +59,8 @@ public class UserDetail implements UserDetails {
     public boolean isEnabled() {
         return UserDetails.super.isEnabled();
     }
+
+    public Long getUserId() {
+        return user.getId();
+    }
 }
