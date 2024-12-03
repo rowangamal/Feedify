1ae86f97f804c21c77286b3df731ee0be3c3095b
Armia Joseph <armia.joseph35@gmail.com>
2024-12-03 15:03:35 +0200

    fix: SignupController error

--- backend/src/main/java/com/example/backend/controllers/SignupController.java
+++ backend/src/main/java/com/example/backend/controllers/SignupController.java
@@ -1,5 +1,6 @@
 package com.example.backend.controllers;
 
+import com.example.backend.dtos.UserSignupDTO;
 import com.example.backend.services.SignupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
