d5d7a9bd808d41253b6579e42f21fe67ac8f7351
Armia Joseph <armia.joseph35@gmail.com>
2024-12-04 05:05:13 +0200

    fix: handlers fixing bugs

--- backend/src/main/java/com/example/backend/services/SignupService.java
+++ backend/src/main/java/com/example/backend/services/SignupService.java
@@ -2,11 +2,13 @@ package com.example.backend.services;
 
 import com.example.backend.dtos.UserSignupDTO;
 import com.example.backend.entities.User;
+import com.example.backend.exceptions.UserAlreadyExistException;
 import com.example.backend.signUp.InvalidEmailHandler;
 import com.example.backend.signUp.SignupHandler;
 import com.example.backend.signUp.UserAlreadyExistHandler;
 import com.example.backend.signUp.UsernameTakenHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -16,13 +18,20 @@ public class SignupService {
     private UserService userService;
 
     public void signup(UserSignupDTO userSignupDTO) {
-        SignupHandler handlerChain = new UsernameTakenHandler(userService)
+        SignupHandler handlerChain = new InvalidEmailHandler()
                 .setNextHandler(new UserAlreadyExistHandler(userService))
-                .setNextHandler(new InvalidEmailHandler());
+                .setNextHandler(new UsernameTakenHandler(userService));
+
         handlerChain.handleRequest(userSignupDTO);
-        User user = createUserFromDTO(userSignupDTO);
-        userService.saveUser(user);
+
+        try {
+            User user = createUserFromDTO(userSignupDTO);
+            userService.saveUser(user);
+        } catch (DataIntegrityViolationException e) {
+            throw new UserAlreadyExistException("Email already in use");
+        }
     }
+
 
     private User createUserFromDTO(UserSignupDTO userSignupDTO){
         User user = new User();
