0e7a3e48648efcca9ab2a5cdb7d4ab712aba4d50
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-12-14 23:20:36 +0200

    feat: services and controllers for admin actions on reports

---
+++ backend/src/main/java/com/example/backend/services/ReportPostService.java
@@ -0,0 +1,57 @@
+package com.example.backend.services;
+
+import com.example.backend.dtos.ReportPostDTO;
+import com.example.backend.entities.Post;
+import com.example.backend.entities.ReportPost;
+import com.example.backend.exceptions.PostNoFoundException;
+import com.example.backend.exceptions.ReportNotFoundException;
+import com.example.backend.repositories.PostRepository;
+import com.example.backend.repositories.ReportPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ReportPostService {
+    @Autowired
+    private ReportPostRepository reportPostRepository;
+    @Autowired
+    private PostRepository postRepository;
+
+
+    public List<ReportPostDTO> getAllPostReports() {
+        List<ReportPost> reportPosts = reportPostRepository.findByOrderByCreatedAtDesc();
+        List<ReportPostDTO> reportPostDTOs = new ArrayList<>();
+        for (ReportPost reportPost : reportPosts) {
+            reportPostDTOs.add(new ReportPostDTO(reportPost.getId(),
+                    reportPost.getPost().getId(),
+                    reportPost.getUser().getId(),
+                    reportPost.getUser().getUsername(),
+                    reportPost.getReason(),
+                    reportPost.getCreatedAt())
+            );
+        }
+        return reportPostDTOs;
+    }
+
+//    @Transactional
+    public void deletePost(long reportID){
+        ReportPost reportPost = reportPostRepository.findReportPostById(reportID);
+        if (reportPost == null)
+            throw new ReportNotFoundException("Report not found");
+        Post post = reportPost.getPost();
+        if (post == null)
+            throw new PostNoFoundException("Post not found");
+        postRepository.delete(post);
+
+    }
+
+    public void denyReport(long reportID){
+        ReportPost reportPost = reportPostRepository.findReportPostById(reportID);
+        if (reportPost == null)
+            throw new ReportNotFoundException("Report not found");
+        reportPostRepository.delete(reportPost);
+    }
+}
---
+++ backend/src/main/java/com/example/backend/services/ReportUserService.java
@@ -0,0 +1,55 @@
+package com.example.backend.services;
+
+import com.example.backend.dtos.ReportUserDTO;
+import com.example.backend.entities.ReportUser;
+import com.example.backend.exceptions.ReportNotFoundException;
+import com.example.backend.exceptions.UserNotFoundException;
+import com.example.backend.repositories.ReportUserRepository;
+import com.example.backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ReportUserService {
+    @Autowired
+    private ReportUserRepository reportUserRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<ReportUserDTO> getAllUserReports() {
+        List<ReportUser> reportUsers = reportUserRepository.findByOrderByCreatedAtDesc();
+        List<ReportUserDTO> reportUserDTOs = new ArrayList<>();
+        for (ReportUser reportUser : reportUsers) {
+            reportUserDTOs.add(new ReportUserDTO(reportUser.getId(),
+                    reportUser.getReporter().getId(),
+                    reportUser.getReported().getId(),
+                    reportUser.getReporter().getUsername(),
+                    reportUser.getReported().getUsername(),
+                    reportUser.getReason())
+            );
+        }
+        return reportUserDTOs;
+    }
+
+//    @Transactional
+    public void deleteUser(long reportID){
+        ReportUser reportUser = reportUserRepository.findReportUserById(reportID);
+        if (reportUser == null)
+            throw new ReportNotFoundException("Report not found");
+        if (reportUser.getReported() == null)
+            throw new UserNotFoundException("User not found");
+//        if(reportUser.getReporter().getId() == reportUser.getReported().getId())
+//            throw new ReportNotFound("You can't report yourself");
+        userRepository.delete(reportUser.getReported());
+    }
+
+    public void denyReport(long reportID){
+        ReportUser reportUser = reportUserRepository.findReportUserById(reportID);
+        if (reportUser == null)
+            throw new ReportNotFoundException("Report not found");
+        reportUserRepository.delete(reportUser);
+    }
+}
