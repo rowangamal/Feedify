ba62893afdf16f6be702bd64b20a67ed5193eeb9
Omar-Eldawy <eng.omar.aldawy@gmail.com>
2024-12-17 20:21:15 +0200

    test: feed test 2

--- backend/src/main/java/com/example/backend/controllers/FeedController.java
+++ backend/src/main/java/com/example/backend/controllers/FeedController.java
@@ -4,14 +4,12 @@ import com.example.backend.dtos.FeedDTO;
 import com.example.backend.dtos.PostsResponseDTO;
 import com.example.backend.entities.Post;
 import com.example.backend.services.FeedService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Slf4j
 @RestController
 @RequestMapping("/userProfile")
 @CrossOrigin
@@ -42,7 +40,6 @@ public class FeedController {
         try {
             return ResponseEntity.ok(feedService.getTopicsFeed(feedDTO));
         } catch (Exception e) {
-            log.error("e: ", e);
             return ResponseEntity.badRequest().build();
         }
     }
