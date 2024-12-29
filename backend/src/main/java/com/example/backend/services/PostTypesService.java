6a5f261ac7a97391f268834ba2f2b9afe22854e7
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-12-30 00:41:29 +0200

    feat: admin topic add/delete/get done backend

--- backend/src/main/java/com/example/backend/services/PostTypesService.java
+++ backend/src/main/java/com/example/backend/services/PostTypesService.java
@@ -2,6 +2,8 @@ package com.example.backend.services;
 
 
 import com.example.backend.entities.PostType;
+import com.example.backend.exceptions.TopicAlreadyExistsException;
+import com.example.backend.exceptions.TopicNotFoundException;
 import com.example.backend.repositories.TopicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,4 +17,37 @@ public class PostTypesService {
     public List<PostType> getPostTypes() {
         return topicRepository.findAll();
     }
+
+    public PostType getPostTypeById(long id) {
+        return topicRepository.findById(id);
+    }
+
+    public List<PostType> getAllTopics() {
+        return topicRepository.findAll();
+    }
+
+    public void addTopic(String postTopic) {
+        PostType postType = new PostType();
+        postType.setName(postTopic);
+        if (topicRepository.existsByName(postType.getName())) {
+            throw new TopicAlreadyExistsException("Topic already exists");
+        }
+        topicRepository.save(postType);
+    }
+
+    public void deleteTopic(long postTypeId) {
+        if (!topicRepository.existsById(postTypeId)) {
+            throw new TopicNotFoundException("Topic does not exist");
+        }
+        String[] topics = {"Sport", "Technology", "Health", "Religion", "Troll", "Politics", "Personal"};
+        for (String topic : topics) {
+            if (topicRepository.existsByName(topic)) {
+                if (topicRepository.findById(postTypeId).getName().equals(topic)) {
+                    throw new RuntimeException("Cannot delete default topic");
+                }
+            }
+        }
+        topicRepository.deleteById(postTypeId);
+    }
+
 }
