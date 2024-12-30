051d3283df77e65f644b581953db628e4c9fdb25
Armia Joseph <armia.joseph35@gmail.com>
2024-12-30 23:44:34 +0200

    feat: added comment repository

--- backend/src/main/java/com/example/backend/repositories/CommentRepository.java
+++ backend/src/main/java/com/example/backend/repositories/CommentRepository.java
@@ -1,8 +1,19 @@
 package com.example.backend.repositories;
 
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository {
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO comment (content, created_at, post_id, user_id) VALUES (:content, :createdAt, :postId, :userId)", nativeQuery = true)
+    void addComment(String content, long postId, long userId);
 
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM comment WHERE id = :commentId", nativeQuery = true)
+    int removeComment(long commentId);
 }
