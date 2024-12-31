a31ca4c31039db391454d35e1974cf6cfebfc092
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-12-31 12:39:40 +0200

    chore: tests are done

--- backend/src/test/java/com/example/backend/services/JWTBlacklistServiceTest.java
+++ backend/src/test/java/com/example/backend/services/JWTBlacklistServiceTest.java
@@ -1,56 +1,61 @@
-//package com.example.backend.services;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-//import org.springframework.data.redis.core.StringRedisTemplate;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-////@SpringBootTest
-//class JWTBlacklistServiceTest {
-//
-//
-//
+package com.example.backend.services;
+
+import com.example.backend.exceptions.JWTBlacklistedException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class JWTBlacklistServiceTest {
+
+
 //    @Mock
-//    private StringRedisTemplate redisTemplate;
-//
-//
+    private StringRedisTemplate redisTemplate;
+
 //    @InjectMocks
-//    private JWTBlacklistService jwtBlacklistService;
+    private JWTBlacklistService jwtBlacklistService;
+
+    @BeforeEach
+    void setUp() {
+        jwtBlacklistService = new JWTBlacklistService();
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+        connectionFactory.setHostName("localhost");
+        connectionFactory.setPort(6379);
+        connectionFactory.afterPropertiesSet();
 //
-////    @Override
-////    public void afterPropertiesSet() throws Exception {
-//////        this.jwtBlacklistService = new JWTBlacklistService();
-//////        this.redisTemplate = new StringRedisTemplate();
-//////        jwtBlacklistService.setRedisTemplate(redisTemplate);
-////        MockitoAnnotations.openMocks(this);
-////        jwtBlacklistService.setEXPIRATION_DAYS(7L);
-////    }
-//    @BeforeEach
-//    void setUp() {
-//        jwtBlacklistService = new JWTBlacklistService();
-//        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
-//        connectionFactory.setHostName("localhost");
-//        connectionFactory.setPort(6379);
-//        connectionFactory.afterPropertiesSet();
-//
-//        redisTemplate = new StringRedisTemplate(connectionFactory);
-//        jwtBlacklistService.setRedisTemplate(redisTemplate);
-//        jwtBlacklistService.setEXPIRATION_DAYS(7L);
-//    }
-//
-//    @Test
-//    void testBlacklistToken() {
-//        assertDoesNotThrow(() -> jwtBlacklistService.BlacklistToken("token"));
-//    }
-//
-////    @Test
-////    void testIsTokenBlacklisted() {
-////        assertFalse(jwtBlacklistService.isTokenBlacklisted("token"));
-////    }
-//
-//}
+
+        redisTemplate = new StringRedisTemplate(connectionFactory);
+        jwtBlacklistService.setRedisTemplate(redisTemplate);
+//        MockitoAnnotations.openMocks(this);
+        jwtBlacklistService.setEXPIRATION_DAYS(7L);
+    }
+
+    @Test
+    void testBlacklistToken() {
+        assertDoesNotThrow(() -> jwtBlacklistService.BlacklistToken("token blacklisted"));
+        redisTemplate.delete("token blacklisted");
+    }
+
+    @Test
+    void testBlacklistTokenAlreadyBlacklisted() {
+        redisTemplate.opsForValue().set("token", "blacklisted", 7, TimeUnit.DAYS);
+        assertThrows(JWTBlacklistedException.class, () -> jwtBlacklistService.BlacklistToken("token"));
+    }
+
+    @Test
+    void testIsTokenBlacklisted() {
+        redisTemplate.opsForValue().set("token", "blacklisted", 7, TimeUnit.DAYS);
+        assertTrue(jwtBlacklistService.isTokenBlacklisted("token"));
+    }
+
+
+
+}
