48c63b8c0e47050c88e5f09e5c6e2c3222819056
RafyHany <rafyhany1@gmail.com>
2024-12-31 10:09:43 +0200

    fix: constructor UserInfo

--- backend/src/main/java/com/example/backend/dtos/UserInfoDTO.java
+++ backend/src/main/java/com/example/backend/dtos/UserInfoDTO.java
@@ -9,4 +9,8 @@ public class UserInfoDTO {
     public String username;
     public String profilePic;
     public long id;
+    public UserInfoDTO(String username, String profilePic) {
+        this.username = username;
+        this.profilePic = profilePic;
+    }
 }
